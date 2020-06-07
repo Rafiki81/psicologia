e458713f3550fad41b27a86e59eaf63548c377a3
Rafiki81 <rperezbeato@gmail.com>
2020-06-07 08:57:05 +0200

    Añadido método login para PsicologoService y su implementacion.

--- src/main/java/com/rafiki/psicologia/services/PsicologoService.java
+++ src/main/java/com/rafiki/psicologia/services/PsicologoService.java
@@ -5,6 +5,7 @@ import com.rafiki.psicologia.entities.Psicologo;
 public interface PsicologoService {
 
     Psicologo findById(Long id);
+    Psicologo login(String email, String password);
     Psicologo save(Psicologo psicologo);
     void delete(Psicologo psicologo);
 
--- src/main/java/com/rafiki/psicologia/services/PsicologoServiceImpl.java
+++ src/main/java/com/rafiki/psicologia/services/PsicologoServiceImpl.java
@@ -5,14 +5,26 @@ import com.rafiki.psicologia.repositories.PsicologoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PsicologoServiceImpl implements PsicologoService {
+
     @Autowired
     PsicologoRepository repository;
 
     @Override
     public Psicologo findById(Long id) {
         return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Psicologo login(String email, String password) {
+
+        Predicate<Psicologo> emailMatch = p ->p.getEmail().equals(email);
+        Predicate<Psicologo> passwordMatch = p ->p.getPassword().equals(password);
+        List<Psicologo> psicologos = (List<Psicologo>)repository.findAll();
+        return psicologos.stream().filter(emailMatch).filter(passwordMatch).findFirst().orElse(null);
     }
 
     @Override
