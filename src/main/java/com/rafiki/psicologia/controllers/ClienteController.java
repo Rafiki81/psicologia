fed70df6d5ad10f6a8015d73dea80b573074bc3d
rpere <rperezbeato@gmail.com>
2020-06-15 22:04:30 +0200

    PsicologoController implementado

--- src/main/java/com/rafiki/psicologia/controllers/ClienteController.java
+++ src/main/java/com/rafiki/psicologia/controllers/ClienteController.java
@@ -1,8 +1,22 @@
 package com.rafiki.psicologia.controllers;
 
+import com.rafiki.psicologia.entities.Cliente;
+import com.rafiki.psicologia.repositories.ClienteRepository;
+import com.rafiki.psicologia.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ClienteController {
-    //TODO
+    @Autowired
+    ClienteService service;
+
+    @GetMapping("/clientes")
+    List<Cliente> all() {
+        return service.findAll();
+    }
+
 }
