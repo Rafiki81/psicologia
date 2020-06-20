8a12dc595f6c184f6d2e91855386904b66cc3092
rpere <rperezbeato@gmail.com>
2020-06-20 13:44:40 +0200

    ResponseBody añadido

--- src/main/java/com/rafiki/psicologia/controllers/IndexController.java
+++ src/main/java/com/rafiki/psicologia/controllers/IndexController.java
@@ -5,8 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@ResponseBody
 public class IndexController {
 
     @GetMapping("/")
--- src/main/java/com/rafiki/psicologia/controllers/PsicologoController.java
+++ src/main/java/com/rafiki/psicologia/controllers/PsicologoController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@ResponseBody
 public class PsicologoController {
 
     @Autowired
