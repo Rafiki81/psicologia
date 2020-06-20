82a463fbb13d295c9267b0ebfe12140360e49be2
rpere <rperezbeato@gmail.com>
2020-06-20 14:07:40 +0200

    Anotado repositorio

--- src/main/java/com/rafiki/psicologia/repositories/PsicologoRepository.java
+++ src/main/java/com/rafiki/psicologia/repositories/PsicologoRepository.java
@@ -2,7 +2,9 @@ package com.rafiki.psicologia.repositories;
 
 import com.rafiki.psicologia.entities.Psicologo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface PsicologoRepository extends CrudRepository<Psicologo,Long> {
 
 
